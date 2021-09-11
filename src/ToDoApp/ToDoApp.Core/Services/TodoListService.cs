590768768d6bcae9c0bf2f77e4c4cb8af88cbe9f
GintasS <svedgintas@gmail.com>
2021-09-11 17:05:14 +0300

    Added ToDoListService.

---
+++ src/ToDoApp/ToDoApp.Core/Services/TodoListService.cs
@@ -0,0 +1,35 @@
+﻿using ToDoApp.Core.Interfaces;
+using ToDoApp.Core.Models.Requests;
+using ToDoApp.Core.Models.Responses;
+using ToDoApp.Database;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Core.Services
+{
+    public class ToDoListService : IToDoListService
+    {
+        private readonly TodoAppContext _context;
+
+        public ToDoListService(TodoAppContext context)
+        {
+            _context = context;
+        }
+
+        public CreateToDoListResponseModel CreateEmptyTodoList(CreateToDoListRequestModel requestModel)
+        {
+            var createdEntity = _context.TodoListEntities.Add(new TodoListEntity()
+            {
+                Name = requestModel.Name
+            }).Entity;
+
+            _context.SaveChanges();
+
+            return new CreateToDoListResponseModel
+            {
+                Id = createdEntity.Id,
+                Name = createdEntity.Name
+            };
+
+        }
+    }
+}
