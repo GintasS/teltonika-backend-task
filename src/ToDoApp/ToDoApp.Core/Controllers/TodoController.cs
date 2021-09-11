cac9ea6f8d275dffb37d7c00d5cd64e955d51807
GintasS <svedgintas@gmail.com>
2021-09-11 15:06:14 +0300

    Added Core Project.

---
+++ src/ToDoApp/ToDoApp.Core/Controllers/TodoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using ToDoApp.Database;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly ILogger<TodoController> _logger;
+        private readonly TodoAppContenxt _context;
+
+        public TodoController(ILogger<TodoController> logger, TodoAppContenxt context)
+        {
+            _logger = logger;
+            _context = context;
+            DataSeeder.InsertTodoData(_context);
+        }
+
+        [Route("/helloWorld")]
+        [AllowAnonymous]
+        [HttpGet]
+        public IEnumerable<ToDoSingleItemEntity> Get()
+        {
+            var t = _context.ToDoSingleItemEntities.ToList();
+            return t;
+        }
+
+
+
+
+    }
+}
