68e275c70e5ba82b4097e294fa5c94c58bbde4af
GintasS <svedgintas@gmail.com>
2021-09-11 17:01:16 +0300

    Added API Request models.

---
+++ src/ToDoApp/ToDoApp.Core/Models/Requests/CreateToDoListRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Core.Models.Requests
+{
+    public class CreateToDoListRequestModel
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<ToDoSingleItemEntity> ToDoSingleItemEntities { get; set; }
+    }
+}
