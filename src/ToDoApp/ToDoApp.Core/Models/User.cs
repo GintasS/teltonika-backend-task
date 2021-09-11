c9cef51673fa2097502534430c7186eea57d23d3
GintasS <svedgintas@gmail.com>
2021-09-11 22:34:10 +0300

    Added properties to User model.

--- src/ToDoApp/ToDoApp.Core/Models/User.cs
+++ src/ToDoApp/ToDoApp.Core/Models/User.cs
@@ -1,6 +1,16 @@
-﻿namespace ToDoApp.Core.Models
+﻿using System.Text.Json.Serialization;
+using ToDoApp.Database;
+
+namespace ToDoApp.Core.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        [JsonIgnore]
+        public string Password { get; set; }
+        public Role Role { get; set; }
     }
 }
