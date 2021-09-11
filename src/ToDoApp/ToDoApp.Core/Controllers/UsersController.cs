36fc76784724735c64d9048698b7af3336f72bc3
GintasS <svedgintas@gmail.com>
2021-09-11 22:28:44 +0300

    Added User Controller.

---
+++ src/ToDoApp/ToDoApp.Core/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Core.Interfaces;
+using ToDoApp.Core.Models;
+using ToDoApp.Core.Requests;
+
+namespace ToDoApp.Core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("/authenticate")]
+        public IActionResult Authenticate(AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+
+            if (response == null)
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var users = _userService.GetAllUsers().Select(x => new User
+            {
+                Id = x.Id,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Email = x.Email,
+                Password = x.Password,
+                Role = x.Role
+            });
+
+            return Ok(users);
+        }
+    }
+}
