d6c83fc57fff8ab357317c934d9be8227ed5b1ef
GintasS <svedgintas@gmail.com>
2021-09-11 22:32:52 +0300

    Added Jwt Service.

---
+++ src/ToDoApp/ToDoApp.Core/Interfaces/IJwtService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Database.Entities;
+
+namespace ToDoApp.Core.Interfaces
+{
+    public interface IJwtService
+    {
+        public string GenerateToken(UserEntity user);
+    }
+}
---
+++ src/ToDoApp/ToDoApp.Core/Services/JwtService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Core.Configuration;
+using ToDoApp.Core.Interfaces;
+using ToDoApp.Database.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ToDoApp.Core.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public JwtService(JwtSettings jwtSettings)
+        {
+            _jwtSettings = jwtSettings;
+        }
+
+        public string GenerateToken(UserEntity user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new[] {new Claim("id", user.Id.ToString()) }),
+                Expires = _jwtSettings.ExpirationTime,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
