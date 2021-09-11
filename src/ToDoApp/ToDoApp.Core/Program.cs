40fb1c87b591d712a221b6d30f2ebaa59937dc51
GintasS <svedgintas@gmail.com>
2021-09-11 19:43:45 +0300

    Started using data seeding for DB.

--- src/ToDoApp/ToDoApp.Core/Program.cs
+++ src/ToDoApp/ToDoApp.Core/Program.cs
@@ -1,5 +1,9 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ToDoApp.Database;
 
 namespace ToDoApp.Core
 {
@@ -7,7 +11,28 @@ namespace ToDoApp.Core
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<TodoAppContext>();
+                DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred creating the DB.");
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
@@ -17,8 +42,6 @@ namespace ToDoApp.Core
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-
         }
-
     }
 }
