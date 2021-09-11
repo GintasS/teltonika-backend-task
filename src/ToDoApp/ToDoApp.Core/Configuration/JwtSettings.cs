449e09b1fad59c1ebfb7634c7dfa40645ac73f6d
GintasS <svedgintas@gmail.com>
2021-09-11 22:29:28 +0300

    Added JwtSettings.

---
+++ src/ToDoApp/ToDoApp.Core/Configuration/JwtSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Core.Configuration
+{
+    public class JwtSettings
+    {
+        public string Secret { get; set; }
+        public DateTime ExpirationTime { get; set; } = 
+    }
+}
