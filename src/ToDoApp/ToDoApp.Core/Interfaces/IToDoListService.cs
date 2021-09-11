a0a9e77c43f8b46acb9397449652bf9be270e0c4
GintasS <svedgintas@gmail.com>
2021-09-11 17:03:13 +0300

    Added Interfaces.

---
+++ src/ToDoApp/ToDoApp.Core/Interfaces/IToDoListService.cs
@@ -0,0 +1,10 @@
+﻿using ToDoApp.Core.Models.Requests;
+using ToDoApp.Core.Models.Responses;
+
+namespace ToDoApp.Core.Interfaces
+{
+    public interface IToDoListService
+    {
+        public CreateToDoListResponseModel CreateEmptyTodoList(CreateToDoListRequestModel requestModel);
+    }
+}
