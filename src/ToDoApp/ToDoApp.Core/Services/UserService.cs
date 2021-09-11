13c5fa8597d0e2ec6147f11e402e78ec2496e4a2
GintasS <svedgintas@gmail.com>
2021-09-11 19:43:58 +0300

    Added User service.

---
+++ src/ToDoApp/ToDoApp.Core/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Core.Interfaces;
+
+namespace ToDoApp.Core.Services
+{
+    public class UserService : IUserService
+    {
+        public AuthenticateResponse Authenticate(AuthenticateRequest model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
