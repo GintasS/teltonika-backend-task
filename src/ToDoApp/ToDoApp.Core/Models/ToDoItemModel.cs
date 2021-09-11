fbf536ea530e9d527d13ea7aac9aa76e9bede0cc
GintasS <svedgintas@gmail.com>
2021-09-11 22:34:29 +0300

    Added another TodoItem model.

---
+++ src/ToDoApp/ToDoApp.Core/Models/ToDoItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Core.Models
+{
+    public class ToDoItemModel : ToDoItem
+    {
+        public int ListId { get; set; }
+        public int ItemId { get; set; }
+    }
+}
