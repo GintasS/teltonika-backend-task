1824464e31121c9356ab07f37a0c8f591ee8424f
GintasS <svedgintas@gmail.com>
2021-09-11 22:31:16 +0300

    Added Attributes for entities.

--- src/ToDoApp/ToDoApp.Database/Entities/ToDoItemEntity.cs
+++ src/ToDoApp/ToDoApp.Database/Entities/ToDoItemEntity.cs
@@ -1,8 +1,12 @@
-﻿namespace ToDoApp.Database.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Database.Entities
 {
     public class ToDoItemEntity
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public bool IsDone { get; set; }
         public virtual ToDoListEntity ToDoListEntity { get; set; }
--- src/ToDoApp/ToDoApp.Database/Entities/UserEntity.cs
+++ src/ToDoApp/ToDoApp.Database/Entities/UserEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using Org.BouncyCastle.Asn1.X509;
 
@@ -7,8 +8,16 @@ namespace ToDoApp.Database.Entities
 {
     public class UserEntity
     {
+        [Key]
         public int Id { get; set; }
-        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(12)]
         public string Password { get; set; }
         public Role Role { get; set; }
     }
