770b04fb960bf9be13421b656bbc3262d0f259f0
GintasS <svedgintas@gmail.com>
2021-09-11 19:38:51 +0300

    Renamed DataSeeder to DbInitializer.

---
+++ src/ToDoApp/ToDoApp.Database/DbInitializer.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ToDoApp.Database.Entities;
+
+namespace ToDoApp.Database
+{
+    public class DbInitializer
+    {
+        public static void Initialize(TodoAppContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.ToDoListEntities.Any() || context.ToDoItemEntities.Any())
+            {
+                return;
+            }
+
+            var toDoLists = new List<ToDoListEntity>
+            {
+                new ToDoListEntity
+                {
+                    Name = "School List"
+                },
+                new ToDoListEntity
+                {
+                    Name = "Home List"
+                }
+            };
+
+            var toDoItems = new List<ToDoItemEntity>
+            {
+                new ToDoItemEntity
+                {
+                    Name = "Write an essay.",
+                    ToDoListEntity = toDoLists[0]
+                },
+                new ToDoItemEntity
+                {
+                    Name = "Finish math exercise.",
+                    ToDoListEntity = toDoLists[0]
+                },
+                new ToDoItemEntity
+                {
+                    Name = "Attend classes.",
+                    ToDoListEntity = toDoLists[0]
+                },
+                new ToDoItemEntity
+                {
+                    Name = "Clean bathroom.",
+                    ToDoListEntity = toDoLists[1]
+                },
+                new ToDoItemEntity
+                {
+                    Name = "Scrub floors.",
+                    ToDoListEntity = toDoLists[1]
+                }
+            };
+
+            var users = new List<UserEntity>()
+            {
+                new UserEntity
+                {
+                    Username = "admin",
+                    Password = "admin",
+                    Role = Role.Admin
+                },
+                new UserEntity
+                {
+                    Username = "user",
+                    Password = "user",
+                    Role = Role.User
+                },
+            };
+
+            foreach (var todoList in toDoLists)
+            {
+                context.ToDoListEntities.Add(todoList);
+            }
+
+            foreach (var todoItem in toDoItems)
+            {
+                context.ToDoItemEntities.Add(todoItem);
+            }
+
+            foreach (var user in users)
+            {
+                context.UserEntities.Add(user);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
