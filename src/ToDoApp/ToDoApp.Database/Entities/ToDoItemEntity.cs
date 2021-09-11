5f9bf981466c3d3ffdfde24034dba921cd3e4178
GintasS <svedgintas@gmail.com>
2021-09-11 19:38:05 +0300

    Added more DB Entities.

---
+++ src/ToDoApp/ToDoApp.Database/Entities/ToDoItemEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApp.Database.Entities
+{
+    public class ToDoItemEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+        public virtual ToDoListEntity ToDoListEntity { get; set; }
+    }
+}
---
+++ src/ToDoApp/ToDoApp.Database/Entities/UserEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Org.BouncyCastle.Asn1.X509;
+
+namespace ToDoApp.Database.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
