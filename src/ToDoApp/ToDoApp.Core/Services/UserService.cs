eb933bfea405a35d0691699045f5b5086eba9fb8
GintasS <svedgintas@gmail.com>
2021-09-12 16:24:27 +0300

    Added methods for validating input.

--- src/ToDoApp/ToDoApp.Core/Services/UserService.cs
+++ src/ToDoApp/ToDoApp.Core/Services/UserService.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
 using ToDoApp.Core.Configuration;
 using ToDoApp.Core.Interfaces;
 using ToDoApp.Core.Models;
@@ -18,11 +20,13 @@ namespace ToDoApp.Core.Services
     {
         private readonly TodoAppContext _context;
         private readonly IJwtService _jwtService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public UserService(TodoAppContext context, IJwtService jwtService)
+        public UserService(TodoAppContext context, IJwtService jwtService, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
             _jwtService = jwtService;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public AuthenticateResponse Authenticate(AuthenticateRequest model)
@@ -55,6 +59,25 @@ namespace ToDoApp.Core.Services
         public UserEntity GetUserById(int userId)
         {
             return _context.UserEntities.SingleOrDefault(x => x.Id == userId);
+        }
+
+        public bool AuthenticatedUserHasSpecificList(int listId)
+        {
+            var user = (UserEntity)_httpContextAccessor.HttpContext.Items["User"];
+
+            return UserHasSpecificList(user.Id, listId);
+        }
+
+        public bool UserHasSpecificList(int userId, int listId)
+        {
+            var list = _context.ToDoListEntities.FirstOrDefault(x => x.Id == listId);
+
+            if (list == null)
+            {
+                return false;
+            }
+
+            return list.UserEntity.Id == userId;
         }
     }
 }
