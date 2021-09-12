c501ed3bd2df63bd6209b20005d2b88d5f4749dd
GintasS <svedgintas@gmail.com>
2021-09-12 23:17:22 +0300

    Added PasswordRecovery entities.

---
+++ src/ToDoApp/ToDoApp.Database/Entities/PasswordRecoveryEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ToDoApp.Database.Enums;
+
+namespace ToDoApp.Database.Entities
+{
+    public class PasswordRecoveryEntity
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public virtual UserEntity UserEntity { get; set; }
+        public PasswordRecoveryStatus PasswordRecoveryStatus { get; set; }
+    }
+}
---
+++ src/ToDoApp/ToDoApp.Database/Enums/PasswordRecoveryStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDoApp.Database.Enums
+{
+    public enum PasswordRecoveryStatus
+    {
+        None = 0, Requested, Recovered
+    }
+}
