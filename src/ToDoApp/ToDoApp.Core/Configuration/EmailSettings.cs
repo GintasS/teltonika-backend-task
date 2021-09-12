ecbd93b239383d759e1fe912a1f09548291cc856
GintasS <svedgintas@gmail.com>
2021-09-12 23:19:51 +0300

    Added email settings.

---
+++ src/ToDoApp/ToDoApp.Core/Configuration/EmailSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace ToDoApp.Core.Configuration
+{
+    public class EmailCredentials
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class EmailSettings
+    {
+        public string HostUrl { get; set; }
+        public EmailCredentials EmailCredentials { get; set; }
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
+    }
+}
