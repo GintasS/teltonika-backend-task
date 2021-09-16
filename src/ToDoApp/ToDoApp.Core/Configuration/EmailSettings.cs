33c701ac84acac7cc9fe3f05a2be974d006d5bd6
GintasS <svedgintas@gmail.com>
2021-09-16 16:18:09 +0300

    Added classes for configuration.

--- src/ToDoApp/ToDoApp.Core/Configuration/EmailSettings.cs
+++ src/ToDoApp/ToDoApp.Core/Configuration/EmailSettings.cs
@@ -1,16 +1,23 @@
 ﻿namespace ToDoApp.Core.Configuration
 {
+    public class EmailSettings
+    {
+        public string HostUrl { get; set; }
+        public EmailCredentials EmailCredentials { get; set; }
+        public PasswordRecoveryEmailTemplate PasswordRecoveryEmailTemplate { get; set; }
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
+    }
+
     public class EmailCredentials
     {
         public string Email { get; set; }
         public string Password { get; set; }
     }
 
-    public class EmailSettings
+    public class PasswordRecoveryEmailTemplate
     {
-        public string HostUrl { get; set; }
-        public EmailCredentials EmailCredentials { get; set; }
-        public int Port { get; set; }
-        public bool EnableSsl { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
     }
 }
--- src/ToDoApp/ToDoApp.Core/Configuration/JwtSettings.cs
+++ src/ToDoApp/ToDoApp.Core/Configuration/JwtSettings.cs
@@ -3,5 +3,12 @@
     public class JwtSettings
     {
         public string Secret { get; set; }
+        public TokenGeneration TokenGeneration { get; set; }
+    }
+    public class TokenGeneration
+    {
+        public string ClaimType { get; set; }
+        public int DaysUntilExpiration { get; set; }
+        public string HashingAlgorithm { get; set; }
     }
 }
