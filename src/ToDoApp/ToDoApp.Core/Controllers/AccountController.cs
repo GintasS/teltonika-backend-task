89881254f190557f4b327748348d81e1ab4f7eb3
GintasS <svedgintas@gmail.com>
2021-09-16 16:18:35 +0300

    Renamed controller from admin to accounts.

--- src/ToDoApp/ToDoApp.Core/Controllers/AccountController.cs
+++ src/ToDoApp/ToDoApp.Core/Controllers/AccountController.cs
@@ -1,33 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Core.Configuration;
 using ToDoApp.Core.Interfaces;
 using ToDoApp.Core.Models.Requests;
 using ToDoApp.Database.Enums;
 
 namespace ToDoApp.Core.Controllers
 {
-    [Route("password-recovery/users/{userId:int}")]
-    public class PasswordRecoveryController : ControllerBase
+    [Route("account")]
+    public class AccountController : ControllerBase
     {
         private readonly IUserService _userService;
         private readonly IEmailService _emailService;
         private readonly IPasswordRecoveryService _passwordRecoveryService;
 
-        public PasswordRecoveryController(IUserService userService, IEmailService emailService, IPasswordRecoveryService passwordRecoveryService)
+        public AccountController(IUserService userService, IEmailService emailService, IPasswordRecoveryService passwordRecoveryService)
         {
             _userService = userService;
             _emailService = emailService;
             _passwordRecoveryService = passwordRecoveryService;
         }
 
-
-        [HttpPut("recovery-link")]
+        [ProducesResponseType(typeof(bool), 200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(403)]
+        [Route("password-recovery/{userId:int}")]
+        [HttpPut]
         public IActionResult SendPasswordRecoveryLink(int userId)
         {
             var user = _userService.GetUserById(userId);
-
             if (user == null)
             {
-                return NotFound("User with the provided UserId was not found.");
+                return NotFound(Constants.ErrorMessages.UserWithIdDoesNotExist);
+            }
+            if (user.Role == Role.Admin)
+            {
+                return Forbid(Constants.ErrorMessages.NoPasswordChangeForAdminRole);
             }
 
             _passwordRecoveryService.SetUserPasswordRecoveryStatus(user.Id, PasswordRecoveryStatus.Requested);
@@ -36,26 +43,29 @@ namespace ToDoApp.Core.Controllers
             return Ok();
         }
 
-        [HttpPut("password")]
+        [ProducesResponseType(typeof(bool), 200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        [Route("password/{userId:int}")]
+        [HttpPut]
         public IActionResult ChangePassword(int userId, PasswordRecoveryRequest model)
         {
             var user = _userService.GetUserById(userId);
             if (user == null)
             {
-                return NotFound("User with the provided UserId was not found.");
+                return NotFound(Constants.ErrorMessages.UserWithIdDoesNotExist);
             }
 
             var recoveryStatus = _passwordRecoveryService.GetUserPasswordRecoveryStatus(userId);
             if (recoveryStatus != PasswordRecoveryStatus.Requested)
             {
-                return BadRequest("Password recovery was not requested, please request it first.");
+                return BadRequest(Constants.ErrorMessages.PasswordRecoveryNotInitiated);
             }
 
-            _passwordRecoveryService.SetUserPasswordRecoveryStatus(userId, PasswordRecoveryStatus.Recovered);
+            _passwordRecoveryService.SetUserPasswordRecoveryStatus(userId, PasswordRecoveryStatus.None);
             _passwordRecoveryService.ChangePassword(userId, model);
 
             return Ok();
         }
-
     }
 }
--- src/ToDoApp/ToDoApp.Core/Controllers/AdminController.cs
+++ src/ToDoApp/ToDoApp.Core/Controllers/AdminController.cs
@@ -1,26 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using ToDoApp.Core.Helpers;
 using ToDoApp.Core.Interfaces;
+using ToDoApp.Core.Models;
+using Constants = ToDoApp.Core.Configuration.Constants;
 
 namespace ToDoApp.Core.Controllers
 {
     // TODO:
-    // 1. Unit tests.
-    // 2. Check all requirements in PDF.
-    // 3. Run Resharper.                                                    +
-    // 4. Run SonarQube.
-    // 5. Try to have all configuration set in appsettings.json
-    // 6. Apply all attributes for DB Migrations.
-    // 7. In controllers, add Status code documentation.
-    // 8. Clean up Startup.cs
-    // 9. Add comments where needed.
-    // 10. Remove email credentials from appsettings.json .
-    // 11. Make endpoints to adhere to Restful standards.
+    // 1. Unit tests (if you're going to write them, use Fluent Assertions, Mocks, GWT structure)   +
+    // 2. Check all requirements in PDF.                                                            +
+    // 3. Run Resharper.                                                                            +
+    // 5. Try to have all configuration set in appsettings.json                                     +
+    // 6. Apply all attributes for DB Migrations.                                                   +
+    // 7. In controllers, add Status code documentation.                                            +
+    // 8. Clean up Startup.cs                                                                       +
+    // 9. Add comments where needed.                                                                +
+    // 10. Remove email credentials from appsettings.json .                                         +
+    // 11. Make endpoints to adhere to Restful standards.                                           +
     // 12. Update readme.
     // 13. Use either SingleOrDefault or FirstOrDefault, but not both.
-    
+    // 14. Now every time you launch a program, it will recreate all the data in DB.
+    //     Can this cause errors for us?
+    // 15. Run all possible scenarios to catch bugs manually.
+    // 16. Remove Unused NuGet packages.                                                            +
+    // 17. Make sure that you use "ToDo" format everywhere.
+    // 18. Check naming everywhere.
+    // 19. Create users with hashed passwords and check the hashes instead of passwords.
+    // 19. Add constants file.                                                                      +
+    // 20. Validate that attribute constraints actually work.
+    // 21. Mappings from entity to model and so on.
+    // 22. Consider using async/ await logic.
+    // 23. Can you use a better hashing algorithm for Jwt.
+    // 24. Add proper email template.
+    // 25. Check authorize lock on Swagger documentation. Is this working correctly?
+    // 26. Can you combine several endpoints under one controller, at least to use the same path?
+    // 27. Check Validation for endpoints.
+    // 28. Check if HTTP Methods are implemented correctly.
+    // 29. Add proper data to DB Initializer (hashed passwords, proper emails).
+
     [ApiController]
-    [Route("[controller]")]
+    [Route("users")]
     public class AdminController : ControllerBase
     {
         private readonly IUserService _userService;
@@ -34,68 +54,70 @@ namespace ToDoApp.Core.Controllers
             _toDoListService = toDoListService;
         }
 
-        [Route("/users/{userId:int}/lists/{listId:int}/items")]
         [AuthorizeAdmin]
+        [ProducesResponseType(typeof(IEnumerable<ToDoItem>), 200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(403)]
+        [Route("{userId:int}/lists/{listId:int}/items")]
         [HttpGet]
         public IActionResult ReadAllToDoItemsForSpecificUser(int userId, int listId)
         {
             var user = _userService.GetUserById(userId);
-
             if (user == null)
             {
-                return BadRequest("User with provided UserId does not exist.");
+                return NotFound(Constants.ErrorMessages.UserWithIdDoesNotExist);
             }
 
             var isCorrectListId = _toDoListService.ToDoListExists(listId);
             if (isCorrectListId == false)
             {
-                return BadRequest("List with provided Id does not exist.");
+                return NotFound(Constants.ErrorMessages.ListWithIdDoesNotExist);
             }
 
             var isCorrectUser = _userService.UserHasSpecificList(userId, listId);
             if (isCorrectUser == false)
             {
-                return BadRequest("User does not own this list.");
+                return Forbid(Constants.ErrorMessages.UserDoesNotOwnSpecificList);
             }
 
             var toDoItems = _toDoItemService.ReadAllToDoItems(listId);
             return Ok(toDoItems);
         }
 
-        [Route("/users/{userId:int}/lists/{listId:int}/items/{itemId:int}")]
         [AuthorizeAdmin]
+        [ProducesResponseType(typeof(bool), 200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(403)]
+        [Route("{userId:int}/lists/{listId:int}/items/{itemId:int}")]
         [HttpDelete]
         public IActionResult DeleteTodoItemForSpecificUser(int userId, int listId, int itemId)
         {
             var user = _userService.GetUserById(userId);
-
             if (user == null)
             {
-                return NotFound("User with provided UserId does not exist.");
+                return NotFound(Constants.ErrorMessages.UserWithIdDoesNotExist);
             }
 
             var isCorrectListId = _toDoListService.ToDoListExists(listId);
             if (isCorrectListId == false)
             {
-                return NotFound("List with provided Id does not exist.");
+                return NotFound(Constants.ErrorMessages.ListWithIdDoesNotExist);
             }
 
             var isCorrectUser = _userService.UserHasSpecificList(userId, listId);
             if (isCorrectUser == false)
             {
-                return Forbid("User does not own this list.");
+                return Forbid(Constants.ErrorMessages.UserDoesNotOwnSpecificList);
             }
 
             var todoItem = _toDoItemService.FindToDoItem(itemId);
             if (todoItem == null)
             {
-                return NotFound("List Item was not found.");
+                return NotFound(Constants.ErrorMessages.ToDoItemWithItemIdDoesNotExist);
             }
 
-            var isSucccess = _toDoItemService.DeleteToDoItem(todoItem);
-            return Ok(isSucccess);
+            var isSuccess = _toDoItemService.DeleteToDoItem(todoItem);
+            return Ok(isSuccess);
         }
-
-
     }
 }
