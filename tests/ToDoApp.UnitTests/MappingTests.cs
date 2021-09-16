7582220464f884a34551aea51d3559c949c7044d
GintasS <svedgintas@gmail.com>
2021-09-16 16:13:59 +0300

    Added some unit tests.

---
+++ tests/ToDoApp.UnitTests/MappingTests.cs
@@ -0,0 +1,161 @@
+using FluentAssertions;
+using ToDoApp.Core.Configuration;
+using ToDoApp.Core.Helpers;
+using ToDoApp.Core.Mappings;
+using ToDoApp.Core.Models;
+using ToDoApp.Core.Models.Requests;
+using ToDoApp.Database.Entities;
+using ToDoApp.Database.Enums;
+using Xunit;
+
+namespace ToDoApp.Tests
+{
+    public class MappingTests
+    {
+        private readonly UserEntity _userEntity;
+        private readonly ToDoItemEntity _toDoItemEntity;
+
+        public MappingTests()
+        {
+            _userEntity = new UserEntity
+            {
+                Email = "test@email.com",
+                FirstName = "John",
+                Id = 1,
+                LastName = "Johnny",
+                Password = "123",
+                Role = Role.Admin
+            };
+
+            _toDoItemEntity = new ToDoItemEntity
+            {
+                Id = 1,
+                Name = "Test",
+                IsDone = false,
+                ToDoListEntity = new ToDoListEntity()
+                {
+                    Id = 1
+                }
+            };
+        }
+
+        [Fact]
+        public void Should_Map_UserEntity_To_EmailMessage()
+        {
+            // Given
+            var passwordRecoveryEmailTemplate = new PasswordRecoveryEmailTemplate
+            {
+                Subject = "My Subject",
+                Body = "{0}/test"
+            };
+
+            // When
+            var emailMessage = _userEntity.MapToEmailMessage(passwordRecoveryEmailTemplate);
+
+            // Then
+            emailMessage.Should().NotBeNull();
+            emailMessage.Subject.Should().Be(passwordRecoveryEmailTemplate.Subject);
+            emailMessage.Body.Should().Be(_userEntity.Id + "/test");
+            emailMessage.RecipientEmail.Should().Be(_userEntity.Email);
+        }
+
+        [Fact]
+        public void Should_Map_CreateToDoItemRequest_To_ToDoItemEntity()
+        {
+            // Given
+            var request = new CreateToDoItemRequest
+            {
+                Name = "My Todo Item",
+                IsDone = false
+            };
+
+            var toDoListEntity = new ToDoListEntity();
+
+            // When
+            var toDoItemEntity = request.MapToToDoItemEntity(toDoListEntity);
+
+            // Then
+            toDoItemEntity.Should().NotBeNull();
+            toDoItemEntity.Name.Should().Be(request.Name);
+            toDoItemEntity.IsDone.Should().Be(request.IsDone);
+            toDoItemEntity.ToDoListEntity.Should().BeSameAs(toDoListEntity);
+        }
+
+        [Fact]
+        public void Should_Map_ToDoItemEntity_To_CreateToDoItemResponse()
+        {
+            // When
+            var response = _toDoItemEntity.MapToCreateToDoItemResponse();
+
+            // Then
+            response.Should().NotBeNull();
+            response.Id.Should().Be(_toDoItemEntity.Id);
+            response.Name.Should().Be(_toDoItemEntity.Name);
+            response.IsDone.Should().Be(_toDoItemEntity.IsDone);
+            response.ListId.Should().Be(_toDoItemEntity.ToDoListEntity.Id);
+        }
+
+        [Fact]
+        public void Should_Map_ToDoItemEntity_To_ReadToDoItemResponse()
+        {
+            // When
+            var response = _toDoItemEntity.MapToReadToDoItemResponse();
+
+            // Then
+            response.Should().NotBeNull();
+            response.Id.Should().Be(_toDoItemEntity.Id);
+            response.Name.Should().Be(_toDoItemEntity.Name);
+            response.IsDone.Should().Be(_toDoItemEntity.IsDone);
+        }
+
+        [Fact]
+        public void Should_Map_ToDoItemEntity_To_UpdateToDoItemResponse()
+        {
+            // When
+            var response = _toDoItemEntity.MapToUpdateToDoItemResponse();
+
+            // Then
+            response.Should().NotBeNull();
+            response.Id.Should().Be(_toDoItemEntity.Id);
+            response.Name.Should().Be(_toDoItemEntity.Name);
+            response.IsDone.Should().Be(_toDoItemEntity.IsDone);
+            response.ListId.Should().Be(_toDoItemEntity.ToDoListEntity.Id);
+        }
+
+        [Fact]
+        public void Should_Map_UserEntity_To_AuthenticateResponse()
+        {
+            // Given
+            var token = "token";
+                
+            // When
+            var response = _userEntity.MapToAuthenticateResponse(token);
+
+            // Then
+            response.Should().NotBeNull();
+            response.Id.Should().Be(_toDoItemEntity.Id);
+            response.FirstName.Should().Be(_userEntity.FirstName);
+            response.LastName.Should().Be(_userEntity.LastName);
+            response.Token.Should().Be(token);
+            response.Email.Should().Be(response.Email);
+        }
+
+        [Fact]
+        public void Should_Map_UserEntity_To_User()
+        {
+            // When
+            var user = _userEntity.MapToUser();
+
+            // Then
+            user.Should().NotBeNull();
+            user.Id.Should().Be(_userEntity.Id);
+            user.FirstName.Should().Be(_userEntity.FirstName);
+            user.LastName.Should().Be(_userEntity.LastName);
+            user.Email.Should().Be(_userEntity.Email);
+            user.Password.Should().Be(_userEntity.Password);
+            user.Role.Should().Be(_userEntity.Role);
+        }
+
+
+    }
+}
---
+++ tests/ToDoApp.UnitTests/ServiceTests/JwtServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using Moq;
+using ToDoApp.Core.Configuration;
+using ToDoApp.Core.Interfaces;
+using ToDoApp.Core.Services;
+using ToDoApp.Database.Entities;
+using ToDoApp.Database.Enums;
+using Xunit;
+
+namespace ToDoApp.UnitTests.ServiceTests
+{
+    public class JwtServiceTests
+    {
+        private readonly IJwtService _jwtService;
+        private readonly UserEntity _userEntity;
+
+        public JwtServiceTests()
+        {
+            var jwtOptionsMock = new Mock<IOptionsMonitor<JwtSettings>>();
+            jwtOptionsMock.Setup(x => x.CurrentValue)
+                .Returns(new JwtSettings
+                {
+                    Secret = "TEST SECRET TEST SECRET",
+                    TokenGeneration = new TokenGeneration()
+                    {
+                        ClaimType = "id",
+                        DaysUntilExpiration = 7,
+                        HashingAlgorithm = @"http://www.w3.org/2001/04/xmldsig-more#hmac-sha256"
+                    }
+                });
+
+
+            _jwtService = new JwtService(jwtOptionsMock.Object);
+
+            _userEntity = new UserEntity
+            {
+                Email = "test@email.com",
+                FirstName = "John",
+                Id = 1,
+                LastName = "Johnny",
+                Password = "123",
+                Role = Role.Admin
+            };
+        }
+
+        [Fact]
+        public void Should_Generate_Jwt_Token()
+        {
+            // When
+            var result = _jwtService.GenerateToken(_userEntity);
+
+            // Then
+            result.Should().NotBeNull();
+            result.Length.Should().Be(163);
+            result.Should().StartWith("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9");
+        }
+    }
+}
